037db5e5e566f16ba3b0cc39d05d016a71ac4d58
Jonathan Byrnes <48105542+jonathanbyrnes@users.noreply.github.com>
2025-06-17 15:12:41 +0100

    fix: oops package name wrong

--- src/main/java/core/craft/listener/RedirectListener.java
+++ src/main/java/core/craft/listener/RedirectListener.java
@@ -1,5 +1,6 @@
-package com.example.bedrockredirect;
+package core.craft.listener;
 
+import core.craft.config.CommandRedirectConfig;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
